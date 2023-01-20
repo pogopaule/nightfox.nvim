1c930a07b59bc49812129100638bb149f359192f
pogopaule <pogopaule@posteo.de>
2023-01-20 12:25:07 +0100

    fix(module): SagaBorder and RenameNormal highlight groups for lsgsaga
    Also add all highlight groups that are yet to be adjusted.
    See https://github.com/glepnir/lspsaga.nvim/blob/19744ee0b3c84832d8848754c444b9069c953316/lua/lspsaga/highlight.lua

--- lua/nightfox/group/modules/lsp_saga.lua
+++ lua/nightfox/group/modules/lsp_saga.lua
@@ -3,19 +3,61 @@
 local M = {}
 
 function M.get(spec, config, opts)
+  local colors = spec.palette
   -- stylua: ignore
   return {
-    LspFloatWinNormal          = { bg = spec.bg0 },
-    LspFloatWinBorder          = { fg = spec.fg3 },
-    LspSagaBorderTitle         = { link = "Title" },
-    LspSagaHoverBorder         = { fg = spec.fg3 },
-    LspSagaRenameBorder        = { fg = spec.fg3 },
-    LspSagaDefPreviewBorder    = { fg = spec.fg3 },
-    LspSagaCodeActionBorder    = { fg = spec.fg3 },
-    LspSagaFinderSelection     = { fg = spec.sel0 },
-    LspSagaCodeActionTitle     = { link = "Title" },
-    LspSagaCodeActionContent   = { link = "String" },
-    LspSagaSignatureHelpBorder = { fg = spec.diag.error },
+    -- general
+    -- TitleString        = { bg = colors.title_bg, fg  = colors.black, bold  = true },
+    -- TitleSymbol        = { bg = colors.normal_bg, fg = colors.title_bg },
+    -- TitleIcon          = { bg = colors.title_bg, fg  = colors.red },
+    SagaBorder         = { bg = spec.bg0 },
+    -- SagaExpand         = { fg = colors.blue.base },
+    -- SagaCollapse       = { fg = colors.blue.base },
+    -- SagaBeacon         = { bg = colors.blue.bright },
+    -- code action
+    -- ActionPreviewTitle = { fg = colors.purple, bg    = colors.normal_bg },
+    -- CodeActionText     = { fg = colors.yellow },
+    -- CodeActionConceal  = { fg = colors.green },
+    -- finder
+    -- FinderSelection    = { fg = colors.cyan, bold    = true },
+    -- FinderFileName     = { fg = colors.white },
+    -- FinderIcon         = { fg = colors.cyan },
+    -- FinderType         = { fg = colors.purple },
+    --finder spinner
+    -- FinderSpinnerTitle = { fg = colors.magenta, bold = true },
+    -- FinderSpinner      = { fg = colors.magenta, bold = true },
+    -- FinderVirtText     = { fg = colors.red },
+    -- definition
+    -- hover
+    -- rename
+    RenameNormal       = { fg = spec.fg0, bg = spec.bg0 },
+    -- diagnostic
+    -- DiagnosticSource   = { fg = 'gray' },
+    -- DiagnosticPos      = { fg = colors.gray },
+    -- DiagnosticWord     = { fg = colors.fg },
+    -- Call Hierachry
+    -- CallHierarchyIcon  = { fg = colors.purple },
+    -- CallHierarchyTitle = { fg = colors.red },
+    -- lightbulb
+    -- shadow
+    -- SagaShadow         = { bg = colors.black },
+    -- Outline
+    -- OutlineIndent      = { fg = colors.blue.bright },
+
+
+
+    -- LspFloatWinNormal          = { bg = spec.bg0 },
+    -- LspFloatWinBorder          = { fg = spec.fg3 },
+    -- LspSagaBorderTitle         = { link = "Title" },
+    -- LspSagaHoverBorder         = { fg = spec.fg3 },
+    -- LspSagaRenameBorder        = { fg = spec.fg3 },
+    -- LspSagaDefPreviewBorder    = { fg = spec.fg3 },
+    -- LspSagaCodeActionBorder    = { fg = spec.fg3 },
+    -- LspSagaFinderSelection     = { fg = spec.sel0 },
+    -- LspSagaCodeActionTitle     = { link = "Title" },
+    -- LspSagaCodeActionContent   = { link = "String" },
+    -- LspSagaSignatureHelpBorder = { fg = spec.diag.error },
+    -- RenameBorder               = { bg = "#ff0000" },
     -- ReferencesCount            = { fg = cp.purple },
     -- DefinitionCount            = { fg = cp.purple },
     -- DefinitionIcon             = { fg = cp.blue },
